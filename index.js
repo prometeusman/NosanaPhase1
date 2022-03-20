6a26283f92dcf39561f84c00cba7ef33e9a96082
prometeusman <49728401+prometeusman@users.noreply.github.com>
2022-03-20 09:13:04 +0400

    Create index.js
    Test

---
+++ index.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1.0,user-scalable=no"
+    />
+    <meta http-equiv="x-dns-prefetch-control" content="on" />
+    <link rel="dns-prefetch" href="https://www.fxdx.uk" />
+    <link rel="icon" href="/favicon.ico" />
+    <title>fxdx</title>
+    <script
+      type="text/javascript"
+      src="/charting_library-master/charting_library/charting_library.min.js"
+    ></script>
+    <!-- <script type="text/javascript" src="https://v.vaptcha.com/v3.js"></script> -->
+
+    <script src="https://www.google.com/recaptcha/api.js?render=6Lcfa3waAAAAAGM0blHFiyr_NvDp3NAYjuLsOWPB"></script>
+    <style>
+      .el-table th.el-table__cell>.cell{
+          padding-left: 10px;
+          padding-right: 10px;
+      }
+      .el-notification__content {
+        text-align: left !important;
+      }
+    </style>
+    <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+  <link href="/js/0.1647432509086.js" rel="prefetch"><link href="/js/1.1647432509086.js" rel="prefetch"><link href="/js/2.1647432509086.js" rel="prefetch"><link href="/js/3.1647432509086.js" rel="prefetch"><link href="/js/4.1647432509086.js" rel="prefetch"><link href="/js/5.1647432509086.js" rel="prefetch"><link href="/js/6.1647432509086.js" rel="prefetch"><link href="/js/7.1647432509086.js" rel="prefetch"><link href="/js/8.1647432509086.js" rel="prefetch"><link href="/js/9.1647432509086.js" rel="prefetch"><link href="/js/app.1647432509086.js" rel="preload" as="script"><link href="/js/chunk-vendors.1647432509086.js" rel="preload" as="script"></head>
+  <body>
+    <noscript>
+      <strong
+        >We're sorry but fxdx doesn't work
+        properly without JavaScript enabled. Please enable it to
+        continue.</strong
+      >
+    </noscript>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+  <script type="text/javascript" src="/js/chunk-vendors.1647432509086.js"></script><script type="text/javascript" src="/js/app.1647432509086.js"></script></body>
+</html>
